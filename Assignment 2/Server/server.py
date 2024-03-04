0b16b08b2395749c1ec7d3a02767890312276aea
Anuj <anujkakde24@gmail.com>
2024-03-04 09:44:09 +0530

    Skeleton code

---
+++ Assignment 2/Server/server.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+import random
+
+app = Flask(__name__)
+
+def get_connection():
+    try:
+        # Connection parameters
+        connection = mysql.connector.connect(host='localhost',
+                                             database='your_database_name',
+                                             user='your_username',
+                                             password='your_password')
+        if connection.is_connected():
+            db_info = connection.get_server_info()
+            print(f"Connected to MySQL Server version {db_info}")
+            cursor = connection.cursor()
+            return connection, cursor
+        
+    except Exception as e:
+        print(f"Error: {e}")
+
+def close_connection(connection, cursor):
+    try:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+@app.route('/config', methods=['POST'])
+def config():
+    pass # TODO: Implement this method
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    pass # TODO: Implement this method
+
+@app.route('/copy', methods=['GET'])
+def copy():
+    pass # TODO: Implement this method
+
+@app.route('/read', methods=['POST'])
+def read():
+    pass # TODO: Implement this method
+
+@app.route('/write', methods=['POST'])
+def write():
+    pass # TODO: Implement this method
+
+@app.route('/update', methods=['PUT'])
+def update():
+    pass # TODO: Implement this method
+
+@app.route('/del', methods=['DELETE'])
+def delete():
+    pass # TODO: Implement this method
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, threaded=True)
+
