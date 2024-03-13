55b0c883514c65b1c2a4d6e20d2b85bd36840ea8
geekyquentin <pagotiyashwantkrishna@gmail.com>
2024-03-14 00:49:20 +0530

    added code for all /heartbeat, /copy, /read, /update, /del; TODO: /write

--- Assignment 2/Server/server.py
+++ Assignment 2/Server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, current_app
+from flask import Flask, request, jsonify, g
 import mysql.connector
 import random
 import itertools
@@ -24,47 +24,36 @@ class MySQLConnection:
 
 
 app = Flask(__name__)
-# create a mysql connection pool
-# MYSQL_HOST = 'localhost'
-# MYSQL_USER = 'root'
-# MYSQL_PASSWORD = 'password'
-# MYSQL_DATABASE = 'test'
-# connection_pool = mysql.connector.pooling.MySQLConnectionPool(
-#     pool_name="my_pool",
-#     pool_size=5,
-#     host=MYSQL_HOST,
-#     user=MYSQL_USER,
-#     password=MYSQL_PASSWORD,
-#     database=MYSQL_DATABASE
-# )
+
+# Database configuration
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'password'
+MYSQL_DATABASE = 'stud_test'
+
+# Pooling configuration
+POOL_NAME = "my_pool"
+POOL_SIZE = 5
+
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name=POOL_NAME,
+    pool_size=POOL_SIZE,
+    host=MYSQL_HOST,
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    database=MYSQL_DATABASE
+)
 
 
 @app.before_request
 def before_request():
-    try:
-        connection = mysql.connector.connect(host='localhost',
-                                             database='stud_test',
-                                             user='root',
-                                             password='password')
-        if connection.is_connected():
-            db_info = connection.get_server_info()
-            print(f"Connected to MySQL Server version {db_info}")
-            cursor = connection.cursor()
-            current_app.config['connection'] = connection
-            current_app.config['cursor'] = cursor
-    except mysql.connector.Error as e:
-        print(f"Error: {e}")
-        raise ConnectionError("Failed to connect to MySQL server")
+    g.connection = connection_pool.get_connection()
 
 
 @app.teardown_request
 def teardown_request(exception):
-    connection = current_app.config.get('connection')
-    cursor = current_app.config.get('cursor')
-    if connection and connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+    if hasattr(g, 'connection'):
+        g.connection.close()
 
 
 @app.route('/config', methods=['POST'])
@@ -82,13 +71,12 @@ def config():
 
     if not columns or not dtypes or len(columns) != len(dtypes):
         return jsonify({
-            "message": "columns and dtypes must be provided and have the same length",
+            "message": "columns and dtypes must be provided in schema and have the same length",
             "status": "error"
         }), 400
 
     try:
-        cursor = current_app.config['cursor']
-        connection = current_app.config['connection']
+        cursor = g.connection.cursor()
         for shard in shards:
             query = f"CREATE TABLE IF NOT EXISTS {shard} (\
                         {columns[0]} {map_dtype_to_sql(dtypes[0])} PRIMARY KEY,\
@@ -96,7 +84,7 @@ def config():
                         {columns[2]} {map_dtype_to_sql(dtypes[2])}\
                     )"
             cursor.execute(query)
-            connection.commit()
+            g.connection.commit()
 
     except Exception as e:
         print(f"Error: {e}")
@@ -104,6 +92,8 @@ def config():
             "message": f"Failed to create table for {shards[0]} and {shards[1]}",
             "status": "error"
         }), 500
+    finally:
+        cursor.close()
 
     return jsonify({
         "message": f"Server0:{shards[0]}, Server0:{shards[1]} configured",
@@ -128,28 +118,62 @@ def copy():
 
     shards = paylod['shards']
 
-    response = {}
     try:
-        cursor = current_app.config['cursor']
+        cursor = g.connection.cursor()
 
+        response = {}
         for shard in shards:
             query = f"SELECT * FROM {shard}"
             cursor.execute(query)
             response[shard] = list_to_colmap(cursor)
+
+        response['status'] = 'success'
+        return jsonify(response), 200
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({
             "message": f"Failed to query data",
             "status": "error"
         }), 500
-
-    response['status'] = 'success'
-    return jsonify(response), 200
+    finally:
+        cursor.close()
 
 
 @app.route('/read', methods=['POST'])
 def read():
-    pass  # TODO: Implement this method
+    payload = request.json
+
+    if 'shard' not in payload or 'Stud_id' not in payload:
+        return jsonify({
+            "message": "Payload must contain 'shard' and 'Stud_id' keys",
+            "status": "error"
+        }), 400
+
+    shard, stud_id_range = payload['shard'], payload['Stud_id']
+
+    if 'low' not in stud_id_range or 'high' not in stud_id_range:
+        return jsonify({
+            "message": "Stud_id must contain 'low' and 'high' keys",
+            "status": "error"
+        }), 400
+
+    low_id, high_id = stud_id_range['low'], stud_id_range['high']
+
+    try:
+        cursor = g.connection.cursor()
+
+        query = f"SELECT * FROM {shard} WHERE Stud_id BETWEEN {low_id} AND {high_id}"
+        cursor.execute(query)
+        response = {"data": list_to_colmap(cursor), "status": "success"}
+        return jsonify(response), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({
+            "message": f"Failed to query data",
+            "status": "error"
+        }), 500
+    finally:
+        cursor.close()
 
 
 @app.route('/write', methods=['POST'])
@@ -159,18 +183,95 @@ def write():
 
 @app.route('/update', methods=['PUT'])
 def update():
-    pass  # TODO: Implement this method
+    payload = request.json
+
+    if 'shard' not in payload or 'Stud_id' not in payload or 'data' not in payload:
+        return jsonify({
+            "message": "Payload must contain 'shard', 'Stud_id' and 'data' keys",
+            "status": "error"
+        }), 400
+
+    shard, stud_id, data = payload['shard'], payload['Stud_id'], payload['data']
+
+    if 'Stud_id' not in data or int(data['Stud_id']) != stud_id:
+        return jsonify({
+            "message": "Stud_id must be provided in data and match Stud_id in payload",
+            "status": "error"
+        }), 400
+
+    try:
+        cursor = g.connection.cursor()
+
+        cursor.execute(f"SELECT * FROM {shard} WHERE Stud_id = {stud_id}")
+        if not cursor.fetchone():
+            return jsonify({
+                "message": f"Data entry for Stud_id:{stud_id} not found",
+                "status": "error"
+            }), 404
+
+        update_columns = ', '.join(
+            [f"{key} = '{value}'" for key, value in data.items()])
+        query = f"UPDATE {shard} SET {update_columns} WHERE Stud_id = {stud_id}"
+        cursor.execute(query)
+        g.connection.commit()
+
+        return jsonify({
+            "message": f"Data entry for Stud_id:{stud_id} updated",
+            "status": "success"
+        }), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({
+            "message": f"Failed to update data entry for Stud_id:{stud_id}",
+            "status": "error"
+        }), 500
+    finally:
+        cursor.close()
 
 
 @app.route('/del', methods=['DELETE'])
 def delete():
-    pass  # TODO: Implement this method
+    payload = request.json
+
+    if 'shard' not in payload or 'Stud_id' not in payload:
+        return jsonify({
+            "message": "Payload must contain 'shard' and 'Stud_id' keys",
+            "status": "error"
+        }), 400
+
+    shard, stud_id = payload['shard'], payload['Stud_id']
+
+    try:
+        cursor = g.connection.cursor()
+
+        cursor.execute(f"SELECT * FROM {shard} WHERE Stud_id = {stud_id}")
+        if not cursor.fetchone():
+            return jsonify({
+                "message": f"Data entry for Stud_id:{stud_id} not found",
+                "status": "error"
+            }), 404
+
+        query = f"DELETE FROM {shard} WHERE Stud_id = {stud_id}"
+        cursor.execute(query)
+        g.connection.commit()
+
+        return jsonify({
+            "message": f"Data entry for Stud_id:{stud_id} removed",
+            "status": "success"
+        }), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({
+            "message": f"Failed to remove data entry for Stud_id:{stud_id}",
+            "status": "error"
+        }), 500
+    finally:
+        cursor.close()
 
 
 def list_to_colmap(cursor):
-    desc = cursor.description
-    column_names = [col[0] for col in desc]
-    return [{column_names[i]: row[i] for i in range(len(row))} for row in cursor.fetchall()]
+    return [dict(zip(cursor.column_names, row))
+            for row in cursor.fetchall()]
 
 
 def map_dtype_to_sql(dtype):
