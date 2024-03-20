b11d97fcb88f8e1e18162ecbdc0d660d0904fe71
souvikrana <ranasouvik07@gmail.com>
2024-03-20 08:29:48 +0530

    init endpoint updated

--- Assignment 2/Load Balancer/load_balancer.py
+++ Assignment 2/Load Balancer/load_balancer.py
@@ -4,10 +4,21 @@ import os
 import subprocess
 import random
 from time import sleep
+import threading
+
+
+import requests
 
 app = Flask(__name__)
 sql_connection_pool = None
 MAX_RETRY = 1000
+server_id_to_hostname = dict()
+MAX_TIMEOUT = 10
+server_id_to_shard = dict()
+shard_data = []
+SCHEMA = None
+
+sih_lock = threading.Lock()
 
 def connect_to_sql_server(max_pool_size=5, host='localhost', user='root', password='password', database='mydb'):
     global sql_connection_pool
@@ -38,6 +49,18 @@ def connect_to_sql_server(max_pool_size=5, host='localhost', user='root', passwo
             tries += 1
             print(f"Error occursed while connecting to sql server: {e}")
 
+
+
+def initialize_metadata_tables():
+    connection = sql_connection_pool.get_connection()
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS ShardT(Stud_id_low INT, Shard_id INT, Shard_size INT, Valid_idx INT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS MapT(Shard_id INT, Server_id INT)")
+    # Bring information from ShardT and MapT tables to RAM for faster access
+
+    cursor.close()
+    connection.close()
+
 @app.route('/')
 def index():
     data = "Hello, World!"
@@ -58,6 +81,9 @@ def gett():
 
 @app.route('/init', methods=['POST'])
 def init():
+    global SCHEMA
+    global shard_data
+    global server_id_to_shard
     payload = request.json
 
     if 'shards' not in payload or 'schema' not in payload or 'servers' not in payload:
@@ -67,27 +93,78 @@ def init():
         }), 400
 
     schema, shards,servers = payload['schema'], payload['shards'],payload['servers']
+    SCHEMA = schema
+    shard_data = shards 
+    server_id_to_shard = servers
     print(f"Schema: {schema}, Shards: {shards}, Servers: {servers}")
+    # make request to each server to create the database
+    unsucessful_list=[]
+    for server_id in servers.keys():
+        hostname = server_id_to_hostname[server_id] 
+        print(f"Making request to server {server_id} with hostname {hostname} to create database {schema}")
+        try:
+            response = requests.post(f"http://{hostname}/config", json={"schema": schema,"shards": servers[server]},timeout=MAX_TIMEOUT)
+            if response.status_code != 200:
+                print(f"Error occured while making request to server {server_id} with hostname {hostname} to create database {schema}: {response.json()}")
+                return jsonify({
+                    "message": f"Couldn't configure server {server_id} with hostname {hostname} to create database",
+                    "status": "error"
+                }), 500
+            
+        except requests.exceptions.RequestException as e:
+            print(f"Error occured while making request to server {server_id} with hostname {hostname} to create database {schema}: {e}")
+            unsucessful_list.append(server_id)
+    
+    if len(unsucessful_list)>0:
+        return jsonify({"message": "Failed to configure servers some server to create database", "status": "error","servers":unsucessful_list}), 206
+
+
+         
+
     # insert Data into ShardT Table (Stud id low: Number, Shard id: Number, Shard size:Number, valid idx:Number)
     connection = sql_connection_pool.get_connection()
     cursor = connection.cursor()
     for shard in shards:
-        Stud_id_low, shard_size, shard_id = shard['Stud_id_low'], shard['Shard_size'], shard['shard_id']
+        Stud_id_low, shard_size, shard_id = shard['Stud_id_low'], shard['Shard_size'], shard['Shard_id']
         cursor.execute(f"INSERT INTO ShardT VALUES ({Stud_id_low}, {shard_id}, {shard_size}, 0)")
     # insert Data into MapT table (Shard id: Number, Server id: Number)
-    for server in servers:
-        server_id, shard_id = server['server_id'], server['shard_id']
-        cursor.execute(f"INSERT INTO MapT VALUES ({shard_id}, {server_id})")
+    for server_id in servers.keys():
+        corr_shards = servers[server_id]
+        for shard_id in corr_shards:
+            cursor.execute(f"INSERT INTO MapT VALUES ({shard_id}, {server_id})")
     
+    cursor.close()
+    connection.close()
 
+    return jsonify({"message": "Successfully configured all servers to create database", "status": "success"}), 200
    
 @app.route('/status', methods=['GET'])
 def status():
-    pass # TODO: Implement this method
+    response = dict()
+    # lock
+    with sih_lock:
+        total_servers = len(server_id_to_hostname)
+    # unlock
+
+    response['N'] = total_servers 
+    response['schema'] = SCHEMA
+    response['shards'] = shard_data
+    response['servers'] = server_id_to_shard
+
+    return jsonify(response), 200
+
 
 @app.route('/add', methods=['POST'])
 def add():
-    pass # TODO: Implement this method
+    payload = request.json
+
+    if 'n' not in payload or 'new_shards' not in payload or 'servers' not in payload:
+        return jsonify({
+            "message": "Payload must contain 'n','new_shards' and 'server' keys",
+            "status": "error"
+        }), 400
+
+    schema, shards,servers = payload['schema'], payload['shards'],payload['servers']
 
 @app.route('/rm', methods=['DELETE'])
 def rm():
@@ -106,7 +183,7 @@ read and write locks
     - read lock: multiple clients can read at the same time
     - write lock: only one client can write at a time
  -- Reading task:
-    - acquire read_count lock
+    - acquire read_count_lock
     - increment read_count
     - if read_count == 1
         - acquire write lock
@@ -122,8 +199,6 @@ read and write locks
     - acquire write lock
     - write data
     - release write lock
-
-
 '''
 
 
@@ -146,6 +221,7 @@ def remove_server(container_name):
 if __name__ == '__main__':
     print("Running load balancer...")
     connect_to_sql_server()
+    initialize_metadata_tables()
     print("Creating Servers...")
     for i in range(1,4):
         spawn_server(i, f"server{i}", f"server{i}", 5000+i)
